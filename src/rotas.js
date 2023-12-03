34562abcc41042d13bf029c4245d5b636472297e
Matheus Henrique Bonfim da Silva <104633582+RexDetona@users.noreply.github.com>
2023-12-02 21:18:24 -0300

    Mobile Inteiro
    Funcional

--- src/rotas.js
+++ src/rotas.js
@@ -6,6 +6,7 @@ import { Login } from './pages/Login';
 import { Cadastro } from './pages/Cadastro';
 import { Cadastro2 } from './pages/Cadastro_2';
 import { Cadastro3 } from './pages/Cadastro_3';
+import { ListaFretes } from './pages/ListaFretes';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +18,7 @@ export default function Rotas() {
         <Stack.Screen name="Cadastro" component={Cadastro} />
         <Stack.Screen name="Cadastro2" component={Cadastro2} />
         <Stack.Screen name="Cadastro3" component={Cadastro3} />
+        <Stack.Screen name="ListaFretes" component={ListaFretes} />
       </Stack.Navigator>
     </NavigationContainer>
   );
