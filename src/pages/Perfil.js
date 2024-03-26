39b4b994c771624389aa8de740039343f2bda488
Matheus Henrique Bonfim da Silva <104633582+RexDetona@users.noreply.github.com>
2024-03-26 16:56:28 -0300

    TelaPerfil
    Perfil sendo moldado, ícones de configuração, pesquisa, voltar, perfil, câmera e lápis, foram adicionados e organizados na tela, visando o implemento de suas respectivas funções adiante.

--- src/pages/Perfil.js
+++ src/pages/Perfil.js
@@ -10,12 +10,45 @@ export function Perfil({ navigation }) {
     <TouchableOpacity onPress={() => navigation.navigate('ListaFretes')}><Image source={require('../assets/imagens/voltar.png')} style={styles.voltar}/></TouchableOpacity>
         <Text style={styles.mobyheader}>  Menu</Text>
         </View>
-        <TouchableOpacity onPress={() => navigation.navigate('Perfil')} style={styles.perfil}>
+        <View style={{flexDirection: 'row'}}>
+        <TouchableOpacity onPress={() => navigation.navigate('Perfil')}>
+            <Image
+                source={require('../assets/imagens/configuracao.png')}
+                style={styles.config}
+            />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Perfil')}>
+            <Image
+                source={require('../assets/imagens/lupa.png')}
+                style={styles.buscar}
+            />
+        </TouchableOpacity>
+        </View>
+    </View>
+    <View style={{width: '100%'}}>
+    <View style={{position:'relative', marginVertical: 15, alignItems: 'center'}}>
+    <TouchableOpacity style={styles.perfil}>
             <Image
                 source={require('../assets/imagens/perfil.png')}
                 style={styles.imagemPerfil}
             />
         </TouchableOpacity>
+        <TouchableOpacity style={styles.altFoto}>
+            <Image
+                source={require('../assets/imagens/camera.png')}
+                style={styles.fotoIcon}
+            />
+        </TouchableOpacity>   
+    </View>
+    <View style={styles.nome}>
+      <Text style={styles.textoNome}>H. Romeu Pinto</Text>
+      <TouchableOpacity style={styles.altNome}>
+            <Image
+                source={require('../assets/imagens/lapis.png')}
+                style={styles.lapisIcon}
+            />
+        </TouchableOpacity> 
+    </View>
     </View>
     </View>
   );
@@ -46,9 +79,56 @@ mobyheader: {
   fontWeight: 'bold',
   textAlignVertical: 'center',
 },
-imagemPerfil: {
-  width: 60,
-  height: 60,
+config: {
+  width: 30,
+  height: 30,
+  marginRight:25,
+},
+buscar: {
+  width: 30,
+  height: 30,
+},
+perfil: {
+  display: 'flex',
+  alignItems: 'center',
+  width: 150,
+  height: 150,
   borderRadius: 15,
+},
+imagemPerfil: {
+  width: 150,
+  height: 150,
+  borderRadius: 15,
+},
+altFoto: {
+position: 'absolute',
+left: '55%',
+top: '60%',
+},
+fotoIcon: {
+  width: 65,
+  height: 65,
+},
+nome: {
+  alignSelf: 'center',
+  display: 'flex',
+  alignItems: 'center',
+  width: '65%',
+  height: 40,
+  borderColor: '#2D3F57',
+  borderBottomWidth: 3,
+},
+textoNome: {
+  position: 'relative',
+  fontSize: 23,
+  fontWeight: 'bold',
+},
+altNome: {
+position: 'absolute',
+left: '90%',
+},
+lapisIcon: {
+  width: 23,
+  height: 23,
 },
 });
